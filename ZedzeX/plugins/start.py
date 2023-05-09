f3d7d4e827c3d1f35e8095995abaed2f00273aa9
Massom0097 <121150107+Massom0097@users.noreply.github.com>
2023-05-09 19:48:27 +0530

    Update start.py

--- ZedzeX/plugins/start.py
+++ ZedzeX/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgQAAxkBAAIDhWP5SIDs4vEQOka4ugakEw3cBaU1AAKmEQACpvFxHogoqbI1f9ogLgQ")
+            await message.reply_sticker("CAACAgUAAx0CbnXQKgACDv5kWlW5NOCIkpZc1GTJgXqyNnEKCQACVwoAAqkV0FbSPHKnVFtE7C8E")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
@@ -173,7 +173,7 @@ async def start_comm(client, message: Message, _):
                             text="𝗬𝗼𝘂𝗧𝘂𝗯𝗲", url=f"{link}"
                         ),
                         InlineKeyboardButton(
-                            text="𝗦𝘂𝗽𝗽𝗼𝗿𝘁", url="https://t.me/unique_chat_world"
+                            text="𝗦𝘂𝗽𝗽𝗼𝗿𝘁", url="https://t.me/innocentop"
                         ),
                     ],
                 ]
@@ -202,7 +202,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgQAAxkBAAIDhWP5SIDs4vEQOka4ugakEw3cBaU1AAKmEQACpvFxHogoqbI1f9ogLgQ")
+                await message.reply_sticker("CAACAgUAAx0CbnXQKgACDv5kWlW5NOCIkpZc1GTJgXqyNnEKCQACVwoAAqkV0FbSPHKnVFtE7C8E")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
